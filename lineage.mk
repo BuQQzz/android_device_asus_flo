af69811b3c2b4ce4fa6f5c14dc28d69e90463348
BuQQzz <buqqzz@gmail.com>
2018-05-11 15:31:26 -0400

    added nfc line and candy line

--- lineage.mk
+++ lineage.mk
@@ -12,18 +12,18 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_mini_tablet_wifionly.mk)
+# Inherit some common Candy stuff.
+$(call inherit-product, vendor/candy/config/common_mini_tablet_wifionly.mk)
 
-# Inherit from the common Open Source product configuration
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+# Enhanced NFC
+$(call inherit-product, vendor/candy/config/nfc_enhanced.mk)
 
 # Inherit from hardware-specific part of the product configuration
 $(call inherit-product, device/asus/flo/device.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := flo
-PRODUCT_NAME := lineage_flo
+PRODUCT_NAME := candy_flo
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Nexus 7
 PRODUCT_MANUFACTURER := asus
